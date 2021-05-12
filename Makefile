de8eb747c40c63cabff1a90bfc0e2a2a1a86bc18
Alon@NovaLinux <alonrs@gmail.com>
2021-05-12 19:13:14 +0300

    Makefile functions support variable source extensions

--- Makefile
+++ Makefile
@@ -10,8 +10,8 @@ LDFLAGS  := -lpthread -lm
 include ./functions.mk
 
 # Create rules for all object files
-$(call createmodule,$(LIB_DIR),lib)
-$(call createmodule,$(TST_DIR),tst) 
+$(call createmodule,$(LIB_DIR),c,lib)
+$(call createmodule,$(TST_DIR),c,tst) 
 
 # Search for all objects, executables
 LIB_OBJ:=$(patsubst $(LIB_DIR)/%.c,$(BIN_DIR)/%.o,$(wildcard $(LIB_DIR)/*.c))
--- functions.mk
+++ functions.mk
@@ -1,17 +1,18 @@
-# Create rules for every C file in $(1) in $(BIN_DIR)/objects_($2).mk
+# Create rules for every $(2) file in $(1) in $(BIN_DIR)/objects_($3).mk
 # @param $1 Directory to search C files in
+# @param $2 Extention of source files
 # @param $2 Suffix for generated mk file
-createmodule = $(shell $(CC) $(CFLAGS) -MM $(1)/*.c |               \
-    sed -E "s@^(.*):@$(BIN_DIR)/\1:@g"              |               \
-    awk 'NR>1 && /:/ {                                              \
-             printf "\t$$(CC) $$(CFLAGS) -o $$@ -c %s\n%s\n",       \
-                    "$$(patsubst $(BIN_DIR)/%.o,$(1)/%.c,$$@)", $$0 \
-         } NR==1 || !/:/ {                                          \
-             print $$0                                              \
-         } END {                                                    \
-             printf "\t$$(CC) $$(CFLAGS) -c -o $$@ %s\n",           \
-                    "$$(patsubst $(BIN_DIR)/%.o,$(1)/%.c,$$@)"      \
-         }' > $(BIN_DIR)/objects_$(2).mk)
+createmodule = $(shell $(CC) $(CFLAGS) -MM $(1)/*.$(2) |               \
+    sed -E "s@^(.*):@$(BIN_DIR)/\1:@g"                 |               \
+    awk 'NR>1 && /:/ {                                                 \
+             printf "\t$$(CC) $$(CFLAGS) -o $$@ -c %s\n%s\n",          \
+                    "$$(patsubst $(BIN_DIR)/%.o,$(1)/%.$(2),$$@)", $$0 \
+         } NR==1 || !/:/ {                                             \
+             print $$0                                                 \
+         } END {                                                       \
+             printf "\t$$(CC) $$(CFLAGS) -c -o $$@ %s\n",              \
+                    "$$(patsubst $(BIN_DIR)/%.o,$(1)/%.$(2),$$@)"      \
+         }' > $(BIN_DIR)/objects_$(3).mk)
 
 # Check whether the CPU supports $(1); if so, add the flag $(2) to CFLAGS
 # @param $1 CPU extension to check for
